cdac5973bf78ab510b475f42ff2a717b4ae0c600
Hemant Sharma <107341144+hemantxsharma@users.noreply.github.com>
2024-07-20 02:22:55 +0530

    Add files via upload

---
+++ weather-app/src/components/WeatherSummary.js
@@ -0,0 +1,11 @@
+import { convertToFahrenheit, getWeatherType } from "../weatherUtil";
+
+const WeatherSummary=({currentWeather:{temperature,weatherCode},isCelcius})=>{
+    return(
+        <div>
+            <h1>{isCelcius? `${temperature} °C `:`${convertToFahrenheit(temperature)} °F `} | {getWeatherType(weatherCode)}</h1>
+        </div>
+    );
+}
+
+export default WeatherSummary;
